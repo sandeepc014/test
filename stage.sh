c9ab7be99631dd06adb5f93b92c913a5dea83f27
sandeep kousik <sandeepc@linkstreet.in>
2020-01-25 17:48:23 +0530

    changes

--- stage.sh
+++ stage.sh
@@ -1,6 +1,24 @@
-#!/bin/bash
+pipeline {
+  agent {
+    node {
+      label 'ultron_stage'
+    }
+  }
+  stages {
+    stage('Running script') {
+      steps {
+            sh 'cd /var/www/decomission/test'
+      }
+    }
+  }
 
-cd /var/www/decomission/test
+stages {
+    stage('Running script') {
+      steps {
+            sh 'git fetch --all && git checkout composer.lock && git stash && git checkout v8.4.1 && git stash pop'
+      }
+    }
+  }
 
-git fetch --all && git checkout composer.lock && git stash && git checkout v8.4.1 && git stash pop
 
+}
