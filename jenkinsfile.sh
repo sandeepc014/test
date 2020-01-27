4b5684f5d7ee2e21b6399c18c9df0fba720c55fd
sandeep kousik <sandeepc@linkstreet.in>
2020-01-27 17:52:41 +0530

    jenkinssh

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -4,6 +4,10 @@ cd /var/www/webroots/test/
 
 
 
+git fetch --all && git checkout composer.lock && git stash && git checkout $site_version && git stash pop
+
+sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
+
 sed '6 s/.*/APP_SITE_VERSION="'$site_version'"/' .env
 
 
