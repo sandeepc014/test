4068375b8af94ba937f2edc0b1c95824a88ce204
sandeep kousik <sandeepc@linkstreet.in>
2020-01-27 17:29:30 +0530

    remove

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -2,11 +2,7 @@
 
 cd /var/www/webroots/test/
 
-git fetch --all && git checkout composer.lock && git stash && git checkout v8.7.1 && git stash pop
-
-sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
-
-
+read site_version
 
 sed '6 s/.*/APP_SITE_VERSION="'$site_version'"/' .env
 
