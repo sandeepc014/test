70d75380dab7566324bd5e6173af170cf09469e0
sandeep kousik <sandeepc@linkstreet.in>
2020-01-27 16:44:22 +0530

    space

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -6,7 +6,7 @@ git fetch --all && git checkout composer.lock && git stash && git checkout v8.7.
 
 sudo chown -R ubuntu:www-data storage/ bootstrap/ && sudo chmod -R 775 storage/ bootstrap/
 
-read site_version
+
 
 sed '6 s/.*/APP_SITE_VERSION="'$site_version'"/' .env
 
